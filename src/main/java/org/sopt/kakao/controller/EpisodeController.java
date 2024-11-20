8f07aa3576cdf09bb96f9b8a1abcb689a607f793
yeeun0702 <kimyeeun0702@naver.com>
2024-11-20 15:29:46 +0900

    refactor: 리스트로 감싸지 않고 단일객체로 반환

--- src/main/java/org/sopt/kakao/controller/EpisodeController.java
+++ src/main/java/org/sopt/kakao/controller/EpisodeController.java
@@ -3,7 +3,7 @@ package org.sopt.kakao.controller;
 import lombok.RequiredArgsConstructor;
 import org.sopt.kakao.common.dto.SuccessResponse;
 import org.sopt.kakao.service.EpisodeService;
-import org.sopt.kakao.service.dto.EpisodeListResponse;
+import org.sopt.kakao.service.dto.EpisodeDetailResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,7 +20,7 @@ public class EpisodeController {
 
     @GetMapping("/episodes/{id}")
     private ResponseEntity<SuccessResponse> getDetailEpisode(@PathVariable final long id) {
-        EpisodeListResponse episodes = episodeService.getDetail(id);
-        return ResponseEntity.ok(SuccessResponse.of(episodes));
+        EpisodeDetailResponse episode = episodeService.getDetail(id);
+        return ResponseEntity.ok(SuccessResponse.of(episode));
     }
 }
--- src/main/java/org/sopt/kakao/service/EpisodeService.java
+++ src/main/java/org/sopt/kakao/service/EpisodeService.java
@@ -3,12 +3,9 @@ package org.sopt.kakao.service;
 import lombok.RequiredArgsConstructor;
 import org.sopt.kakao.common.dto.ErrorMessage;
 import org.sopt.kakao.common.exception.AppException;
-import org.sopt.kakao.domain.Episode;
 import org.sopt.kakao.domain.Webtoon;
-import org.sopt.kakao.repository.EpisodeRepository;
 import org.sopt.kakao.repository.WebtoonRepository;
 import org.sopt.kakao.service.dto.EpisodeDetailResponse;
-import org.sopt.kakao.service.dto.EpisodeListResponse;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -19,12 +16,10 @@ public class EpisodeService {
 
     private final WebtoonRepository webtoonRepository;
 
-   public EpisodeListResponse getDetail(final long id){
-       Webtoon webtoon = webtoonRepository.findById(id)
-               .orElseThrow(() -> new AppException(ErrorMessage.WEBTOON_NOT_FOUND));
-       EpisodeDetailResponse response = EpisodeDetailResponse.of(webtoon);
+    public EpisodeDetailResponse getDetail(final long id) {
+        Webtoon webtoon = webtoonRepository.findById(id)
+                .orElseThrow(() -> new AppException(ErrorMessage.WEBTOON_NOT_FOUND));
+        return EpisodeDetailResponse.of(webtoon);
+    }
 
-       // 단일 객체를 리스트로 감싸 반환
-       return new EpisodeListResponse(List.of(response));
-   }
 }
