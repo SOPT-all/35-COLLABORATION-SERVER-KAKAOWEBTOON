afd1acb1799283b103c4c0136a91fff2cd070d3e
yeeun0702 <kimyeeun0702@naver.com>
2024-11-22 00:29:25 +0900

    refactor: String으로 요일 값을 받은 후 Day enum으로 매핑하는 로직 추가

--- src/main/java/org/sopt/kakao/domain/Day.java
+++ src/main/java/org/sopt/kakao/domain/Day.java
@@ -2,15 +2,30 @@ package org.sopt.kakao.domain;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.apache.coyote.BadRequestException;
+import org.sopt.kakao.common.dto.ErrorMessage;
+import org.sopt.kakao.common.exception.AppException;
+
+import java.util.Arrays;
 
 @Getter
 @RequiredArgsConstructor
 public enum Day {
-    MON,
-    TUE,
-    WED,
-    THU,
-    FRI,
-    SAT,
-    SUN
+    MON("MON"),
+    TUE("TUE"),
+    WED("WED"),
+    THU("THU"),
+    FRI("FRI"),
+    SAT("SAT"),
+    SUN("SUN");
+
+    private final String day;
+
+    public static Day fromString(String dayString) {
+        return Arrays.stream(values())
+                .filter(day -> day.day.equalsIgnoreCase(dayString))
+                .findFirst()
+                .orElseThrow(() -> new AppException(ErrorMessage.INVALID_DAY));
+    }
+
 }
