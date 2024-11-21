4af63fc7175ce95ea220dc202d8b66e110844d8d
yeeun0702 <kimyeeun0702@naver.com>
2024-11-22 00:55:42 +0900

    refactor: 요일별 웹툰 목록 조회 리뷰를 반영하여 수정

--- src/main/java/org/sopt/kakao/common/dto/ErrorMessage.java
+++ src/main/java/org/sopt/kakao/common/dto/ErrorMessage.java
@@ -10,9 +10,6 @@ public enum ErrorMessage {
 
     // 예시로 써놓은 것
     WEBTOON_NOT_FOUND(HttpStatus.NOT_FOUND.value(), "ID에 해당하는 웹툰을 찾을 수 없습니다."),
-    // 유효성 검사 실패 메시지 추가
-    CONSTRAINT_VIOLATION(HttpStatus.BAD_REQUEST.value(), "요청 데이터가 유효하지 않습니다."),
-
     INVALID_DAY(HttpStatus.BAD_REQUEST.value(), "잘못된 요일 요청입니다.");
 
     private final int status;
