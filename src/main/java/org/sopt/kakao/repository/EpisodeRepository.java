1150860c8de8d87af9ab6a267ad75d63c9eec2d1
kgy1008 <gayeon1008@g.skku.edu>
2024-11-21 16:07:44 +0900

    feat: 기능 구현

--- src/main/java/org/sopt/kakao/repository/EpisodeRepository.java
+++ src/main/java/org/sopt/kakao/repository/EpisodeRepository.java
@@ -1,9 +1,13 @@
 package org.sopt.kakao.repository;
 
+import java.util.List;
 import org.sopt.kakao.domain.Episode;
+import org.sopt.kakao.domain.Webtoon;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface EpisodeRepository extends JpaRepository<Episode, Long> {
+
+    List<Episode> findAllByWebtoon(Webtoon webtoon);
 }
--- src/main/java/org/sopt/kakao/service/EpisodeService.java
+++ src/main/java/org/sopt/kakao/service/EpisodeService.java
@@ -1,11 +1,16 @@
 package org.sopt.kakao.service;
 
+import java.time.LocalDate;
 import lombok.RequiredArgsConstructor;
 import org.sopt.kakao.common.dto.ErrorMessage;
 import org.sopt.kakao.common.exception.AppException;
+import org.sopt.kakao.domain.Episode;
 import org.sopt.kakao.domain.Webtoon;
+import org.sopt.kakao.repository.EpisodeRepository;
 import org.sopt.kakao.repository.WebtoonRepository;
 import org.sopt.kakao.service.dto.EpisodeDetailResponse;
+import org.sopt.kakao.service.dto.EpisodeResponse;
+import org.sopt.kakao.service.dto.EpisodesResponse;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -15,11 +20,30 @@ import java.util.List;
 public class EpisodeService {
 
     private final WebtoonRepository webtoonRepository;
+    private final EpisodeRepository episodeRepository;
 
     public EpisodeDetailResponse getDetail(final long id) {
-        Webtoon webtoon = webtoonRepository.findById(id)
-                .orElseThrow(() -> new AppException(ErrorMessage.WEBTOON_NOT_FOUND));
+        Webtoon webtoon = findById(id);
         return EpisodeDetailResponse.of(webtoon);
     }
 
+    public EpisodesResponse getEpisodes(final long id) {
+        Webtoon webtoon = findById(id);
+        List<Episode> findEpisodes = findAllEpisode(webtoon);
+
+        LocalDate now = LocalDate.now();
+        List<EpisodeResponse> episodes = findEpisodes.stream()
+                .map(episode -> EpisodeResponse.of(episode, episode.findDayUntilFree(now)))
+                .toList();
+        return new EpisodesResponse(episodes);
+    }
+
+    private Webtoon findById(final long id) {
+        return webtoonRepository.findById(id)
+                .orElseThrow(() -> new AppException(ErrorMessage.WEBTOON_NOT_FOUND));
+    }
+
+    private List<Episode> findAllEpisode(final Webtoon webtoon) {
+        return episodeRepository.findAllByWebtoon(webtoon);
+    }
 }
