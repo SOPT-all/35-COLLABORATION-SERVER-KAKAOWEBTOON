3c8f42114ee11b9696b48098376ac85a29a5faf4
kgy1008 <gayeon1008@g.skku.edu>
2024-11-22 18:05:34 +0900

    fix: 수정

--- src/main/java/org/sopt/kakao/service/WebtoonService.java
+++ src/main/java/org/sopt/kakao/service/WebtoonService.java
@@ -1,29 +1,21 @@
 package org.sopt.kakao.service;
 
-import java.util.EnumSet;
 import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
-
 import lombok.RequiredArgsConstructor;
-import org.sopt.kakao.common.dto.ErrorMessage;
-import org.sopt.kakao.common.exception.AppException;
 import org.sopt.kakao.domain.Day;
-import org.sopt.kakao.domain.Thumbnail;
 import org.sopt.kakao.domain.Webtoon;
-import org.sopt.kakao.repository.ThumbnailRepository;
 import org.sopt.kakao.repository.WebtoonRepository;
-import org.sopt.kakao.service.dto.*;
+import org.sopt.kakao.service.dto.WebtoonDayListResponse;
+import org.sopt.kakao.service.dto.WebtoonDayResponse;
+import org.sopt.kakao.service.dto.WebtoonListResponse;
+import org.sopt.kakao.service.dto.WebtoonSearchResponse;
 import org.springframework.stereotype.Service;
-
-import static org.sopt.kakao.common.dto.ErrorMessage.THUMNAIL_NOT_FOUND;
 
 @Service
 @RequiredArgsConstructor
 public class WebtoonService {
 
     private final WebtoonRepository webtoonRepository;
-    private final ThumbnailRepository thumbnailRepository;
 
     public WebtoonListResponse search(final String title) {
         List<Webtoon> findWebtoons = webtoonRepository.findAllByTitle(title);
@@ -37,29 +29,8 @@ public class WebtoonService {
     public WebtoonDayListResponse findWebtoonsByDay(final String dayString) {
         Day day = Day.convertToDay(dayString);
         List<WebtoonDayResponse> webtoons = webtoonRepository.findByDay(day).stream()
-                .map(webtoon -> WebtoonDayResponse.of(webtoon, findThumbnail(webtoon)))
+                .map(WebtoonDayResponse::of)
                 .toList();
         return new WebtoonDayListResponse(webtoons);
     }
-
-
-    private Optional<Thumbnail> findThumbnail(final Webtoon webtoon) {
-        List<Thumbnail> thumbnail = thumbnailRepository.findByWebtoon(webtoon);
-        return thumbnail.stream().findFirst();
-    }
-
-    public WebtoonRecentViewListResponse getRecentWebtoons() {
-        List<Webtoon> recentWebtoons = webtoonRepository.findTop5ByOrderByIdDesc();
-        List<WebtoonRecentViewResponse> webtoonResponses = recentWebtoons.stream()
-                .map(webtoon -> WebtoonRecentViewResponse.of(webtoon, findImage(webtoon)))
-                .toList();
-        return new WebtoonRecentViewListResponse(webtoonResponses);
-    }
-
-
-    private Optional<Thumbnail> findImage(final Webtoon webtoon) {
-        List<Thumbnail> thumbnails = thumbnailRepository.findByWebtoon(webtoon);
-        return thumbnails.stream().reduce((first, second) -> second);
-    }
-
 }
