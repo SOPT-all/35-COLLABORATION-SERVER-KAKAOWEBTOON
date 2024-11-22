6b65b2fe5473866350703c7a935d4260b07911fb
kgy1008 <gayeon1008@g.skku.edu>
2024-11-22 18:47:51 +0900

    fix: 5개만 보내도록 페이징

--- src/main/java/org/sopt/kakao/repository/ThumbnailRepository.java
+++ src/main/java/org/sopt/kakao/repository/ThumbnailRepository.java
@@ -2,10 +2,15 @@ package org.sopt.kakao.repository;
 
 import org.sopt.kakao.domain.Thumbnail;
 import org.sopt.kakao.domain.Webtoon;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import org.springframework.data.jpa.repository.Query;
 
 public interface ThumbnailRepository extends JpaRepository<Thumbnail, Long> {
     List<Thumbnail> findByWebtoon(Webtoon webtoon);
+
+    @Query("select t from Thumbnail t join fetch t.webtoon ORDER BY t.id DESC")
+    List<Thumbnail> findTop5ByOrderByIdDesc(Pageable pageable);
 }
