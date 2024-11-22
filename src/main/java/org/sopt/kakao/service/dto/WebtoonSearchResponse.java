a6606881d0fa8916550dde2133ffbaebe5878b41
kgy1008 <gayeon1008@g.skku.edu>
2024-11-22 18:18:01 +0900

    fix: 웹툰 검색시 이미지 불러오기 수정

--- src/main/java/org/sopt/kakao/service/dto/WebtoonSearchResponse.java
+++ src/main/java/org/sopt/kakao/service/dto/WebtoonSearchResponse.java
@@ -1,5 +1,7 @@
 package org.sopt.kakao.service.dto;
 
+import java.util.Optional;
+import org.sopt.kakao.domain.Thumbnail;
 import org.sopt.kakao.domain.Webtoon;
 
 public record WebtoonSearchResponse(
@@ -9,8 +11,13 @@ public record WebtoonSearchResponse(
         String genre,
         String promotion
 ) {
-    public static WebtoonSearchResponse of(final Webtoon webtoon) {
-        return new WebtoonSearchResponse(webtoon.getTitle(), webtoon.getAuthor(), webtoon.getImage(),
-                webtoon.getGenre().getDescription(), webtoon.getPromotion());
+    public static WebtoonSearchResponse of(final Webtoon webtoon, final Optional<Thumbnail> thumbnail) {
+        return new WebtoonSearchResponse(
+                webtoon.getTitle(),
+                webtoon.getAuthor(),
+                thumbnail.map(Thumbnail::getImage).orElse(null),
+                webtoon.getGenre().getDescription(),
+                webtoon.getPromotion()
+        );
     }
 }
