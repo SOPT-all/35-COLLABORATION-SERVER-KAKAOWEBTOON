34b76f7e0bee0f7369e62056041eaf3c4f2ddf68
kgy1008 <gayeon1008@g.skku.edu>
2024-11-22 18:05:34 +0900

    fix: 수정

--- src/main/java/org/sopt/kakao/service/dto/WebtoonDayResponse.java
+++ src/main/java/org/sopt/kakao/service/dto/WebtoonDayResponse.java
@@ -1,22 +1,17 @@
 package org.sopt.kakao.service.dto;
 
-import jakarta.annotation.Nullable;
-import org.sopt.kakao.domain.Thumbnail;
 import org.sopt.kakao.domain.Webtoon;
-
-import java.util.Optional;
 
 public record WebtoonDayResponse(
         long id,
         String title,
-        @Nullable
         String image
 ) {
-    public static WebtoonDayResponse of(Webtoon webtoon, Optional<Thumbnail> thumbnail){
+    public static WebtoonDayResponse of(Webtoon webtoon) {
         return new WebtoonDayResponse(
                 webtoon.getId(),
                 webtoon.getTitle(),
-                thumbnail.map(Thumbnail::getImage).orElse(null)
+                webtoon.getImage()
         );
     }
 }
