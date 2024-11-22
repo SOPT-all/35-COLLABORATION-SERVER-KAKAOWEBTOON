2012e181c3a49b665c70f8442e6c4f0de08ffa9c
kgy1008 <gayeon1008@g.skku.edu>
2024-11-22 14:55:53 +0900

    refactor: 경로 수정

--- src/main/java/org/sopt/kakao/controller/WebtoonController.java
+++ src/main/java/org/sopt/kakao/controller/WebtoonController.java
@@ -2,18 +2,14 @@ package org.sopt.kakao.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.sopt.kakao.common.dto.SuccessResponse;
-import org.sopt.kakao.domain.Day;
 import org.sopt.kakao.service.WebtoonService;
 import org.sopt.kakao.service.dto.WebtoonDayListResponse;
-import org.sopt.kakao.service.dto.WebtoonDayResponse;
 import org.sopt.kakao.service.dto.WebtoonListResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -22,13 +18,13 @@ public class WebtoonController {
 
     private final WebtoonService webtoonService;
 
-    @GetMapping("/webtoons")
+    @GetMapping("/webtoons/search")
     private ResponseEntity<SuccessResponse> searchWebtoon(@RequestParam final String title) {
         WebtoonListResponse webtoons = webtoonService.search(title);
         return ResponseEntity.ok(SuccessResponse.of(webtoons));
     }
 
-    @GetMapping("/webtoons/day")
+    @GetMapping("/webtoons")
     public ResponseEntity<SuccessResponse> getWebtoonsByDay(@RequestParam final String day) {
         WebtoonDayListResponse webtoons = webtoonService.findWebtoonsByDay(day);
         return ResponseEntity.ok(SuccessResponse.of(webtoons));
